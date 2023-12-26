80cdc0d86bf015a6a323f1c95e9fd6ce071849fc
saivardhanvemula <saivardhanvemulamncl@gmail.com>
2023-12-27 02:34:08 +0530

    multiplayer added

--- index.js
+++ index.js
@@ -1,20 +1,53 @@
-const start = () => {
-    document.getElementById("start").style.display = "none";
-    document.querySelector("#board").style.display = "grid";
-    document.querySelector(".details > h2").innerHTML = "Player 1 turn"
-    document.querySelector(".details > h2").innerHTML = "Player 1 turn"
-}
 let win = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 5, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [3, 5, 7]]
 let count = 0;
 let list = ["", "", "", "", "", "", "", "", ""]
 let playing = true;
 let p1 = 0;
 let p2 = 0;
+let game=document.querySelector(".game");
 let details = document.querySelector(".details");
+let winner = document.querySelector(".winner");
+let single=document.querySelector("#Single");
+let btns=document.querySelector(".btns");
+
+game.style.display="none";
+
+
+
+
+single.addEventListener("click",()=>{
+start();
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 details.innerHTML = `<h2>Click start to play</h2>
                     <span> Player 1: X Score : ${p1}</span >
                     <span> player 2: O Score : ${p2}</span> `;
-let winner = document.querySelector(".winner");
+
+
+const start = () => {
+    btns.style.display="none";
+    game.style.display="flex"
+    // document.querySelector("#start").style.display = "none";
+    document.querySelector("#board").style.display = "grid";
+    document.querySelector(".details > h2").innerHTML = "Player 1 turn"
+    // document.querySelector(".details > h2").innerHTML = "Player 1 turn"
+}
+
+
 const place = (id) => {
     if (!list[id] && playing) {
         let box = document.getElementById(id)
@@ -50,6 +83,8 @@ const place = (id) => {
         }
     }
 }
+
+
 const reset = () => {
     let boxes = document.getElementsByClassName("box");
     for (let i = 0; i < 9; i++) {
@@ -62,6 +97,8 @@ const reset = () => {
     count = 0
     playing = true;
 }
+
+
 const Validate = () => {
     for (let i = 0; i < 8; i++) {
         if (list[win[i][0]] == "X" && list[win[i][1]] == "X" && list[win[i][2]] == "X") {
@@ -74,16 +111,22 @@ const Validate = () => {
     }
     return false
 }
+
+
 const winline = (id1, id2, id3) => {
     document.getElementById(id1).classList.add("won")
     document.getElementById(id2).classList.add("won")
     document.getElementById(id3).classList.add("won")
 }
+
+
 const updateScore = () => {
     details.innerHTML = `<h2>Click start to play</h2>
     <span> Player 1: X Score : ${p1}</span >
     <span> player 2: O Score : ${p2}</span> `;
 }
+
+
 const reload =()=>{
     location.reload();
 }
